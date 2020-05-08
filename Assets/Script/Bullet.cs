c8dd41d36452a82705f141354bda4ba485e0e0ca
Lukas <Tsuki32@hotmail.com>
2020-05-08 11:16:14 +0200

    Fire Method
    An instantiate function that spawns a bullet prefab, and destroys the enemy via tag detection.

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collider) 
+    {
+        if (Collider.gameObject.tag == "Enemy")
+        {
+            Destroy(Collider.transform.gameObject);
+            Debug.Log("ENEMY WAS DETECTED");
+
+        }
+    }
+
+
+}
--- Assets/Script/Fire.cs
+++ Assets/Script/Fire.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    private Vector3 mousePos;
-    private Vector3 objectPos;
-    public GameObject yourPrefab;
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
 
-    void Update()
+    public void FireBullet()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            mousePos = Input.mousePosition;
-            mousePos.z = 2.0f;
-            objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Instantiate(yourPrefab, objectPos, Quaternion.identity);
-        }
-
+        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
     }
+
+
+
 }
